132c403177b3befbfb65b900f71b16f77dc43741
balsamic <balsamic.bz@gmail.com>
2021-11-23 18:14:18 -0700

    update total

--- src/Home.tsx
+++ src/Home.tsx
@@ -195,7 +195,7 @@ const Home = (props: HomeProps) => {
 
       {wallet && <p>Mint Cost: 0.50 SOL </p>}
 
-      {wallet && <p>ARTIFAKTS Minted: {itemsRedeemed} / {itemsAvailable}</p>}
+      {wallet && <p>ARTIFAKTS Minted: {itemsRedeemed} / {itemsAvailable-1}</p>}
 
       <MintContainer>
         {!wallet ? (
