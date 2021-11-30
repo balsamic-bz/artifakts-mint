26979a0c2257c0bfa1db9142163b4c2e97a7901a
balsamic <balsamic.bz@gmail.com>
2021-11-30 06:41:53 -0700

    final

--- src/Home.tsx
+++ src/Home.tsx
@@ -215,7 +215,7 @@ const Home = (props: HomeProps) => {
               )
             ) : (
               <Countdown
-                date={1637427600000}  // JS timestampes are in milliseconds, must have 3 zeros added
+                date={1638291600000}  // JS timestampes are in milliseconds, must have 3 zeros added
                 onMount={({ completed }) => completed && setIsActive(true)}
                 onComplete={() => setIsActive(true)}
                 renderer={renderCounter}
